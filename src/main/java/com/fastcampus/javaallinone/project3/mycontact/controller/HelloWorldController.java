20b0ab38d6541da01fd1140c93bdfca0f71ce4a1
kwangminini <rhkdals7362@gmail.com>
2020-06-17 01:30:19 +0900

    MockMvc Test

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/HelloWorldController.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/controller/HelloWorldController.java
@@ -1,9 +1,7 @@
 package com.fastcampus.javaallinone.project3.mycontact.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //@Controller
 //@ResponseBody
